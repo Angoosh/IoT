e9e4875bc9160351c9a0f7d36995db2a663c1aef
Angoosh <kaplanantonin@gmail.com>
2018-04-04 23:25:29 +0200

    added recievers e-mail addressment

--- Security-NFC/usersetup.py
+++ Security-NFC/usersetup.py
@@ -1,9 +1,12 @@
 import pickle
 import base64
 
-m = str(input("Zadej e-mail: "))
-x = str(input("Zadej heslo: "))
+m = str(input("Zadej e-mail odesilatele: "))
+mr = str(input("Zadej e-mail prijemce: "))
+x = str(input("Zadej heslo odesilatele: "))
 userpass = base64.b64encode(x.encode("utf-8"))
 usermail = m
+reciever = mr
 pickle.dump(userpass, open("psswrd.p", "wb"))
 pickle.dump(usermail, open("mail.p", "wb"))
+pickle.dump(reciever, open("mailR.p", "wb"))
